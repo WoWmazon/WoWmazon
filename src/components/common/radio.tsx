1ab89e46f152ada3aed4d03645832c475c98b884
ljs614 <sjl0614@gmail.com>
2024-10-10 11:03:42 +0900

    ♻️ WOW-17-smart-commit-test #done #comment refactor: radio 코드 수정

--- src/components/common/radio.tsx
+++ src/components/common/radio.tsx
@@ -16,22 +16,6 @@ export default function Radio() {
           Default radio
         </label>
       </div>
-      <div className="flex items-center">
-        <input
-          checked
-          id="default-radio-2"
-          type="radio"
-          value=""
-          name="default-radio"
-          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-        />
-        <label
-          htmlFor="default-radio-2"
-          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          Checked state
-        </label>
-      </div>
     </div>
   );
 }
