44ce104fd1d4ac94eef234e01b9884d5de084dd7
hayoung <wendy4332@naver.com>
2024-10-16 00:45:44 +0900

    ➕ [WOW-25] feat : iconButton 컴포넌트 생성

---
+++ src/components/common/custom-icon-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useState, ButtonHTMLAttributes } from "react";
+import Image from "next/image";
+
+const IconButton = ({
+  icon,
+  activeIcon,
+  size,
+  alt,
+  onClick,
+  defaultActive = false,
+  ...buttonProps
+}: IconButtonProps) => {
+  const [isActive, setIsActive] = useState<boolean>(defaultActive);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsActive((prev) => !prev);
+    if (onClick) onClick(e);
+  };
+
+  return (
+    <button onClick={handleClick} {...buttonProps} className="cursor-pointer">
+      <Image
+        src={isActive && activeIcon ? activeIcon : icon}
+        width={size}
+        height={size}
+        alt={alt}
+        className="cursor-pointer"
+      />
+    </button>
+  );
+};
+
+export default IconButton;
