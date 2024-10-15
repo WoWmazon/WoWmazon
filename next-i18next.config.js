f4d1067d753661ac1b9df47ea45ca6fa9d30195a
재희 <hareppy329@gmail.com>
2024-10-15 17:14:53 +0900

    🔥 WOW-24 remove : 불필요한 파일 삭제

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-i18next').UserConfig} */
-module.exports = {
-  i18n: {
-    defaultLocale: "kr",
-    locales: ["en", "kr"],
-  },
-  localePath: "./public/locales", // 번역 파일 경로
-};
