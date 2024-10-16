f6e9353337b1f3acf0fe07694ed1b04f24c72b8f
hayoung <wendy4332@naver.com>
2024-10-16 16:55:33 +0900

    ➕[WOW-29] feat : badge 컴포넌트 생성

---
+++ src/components/common/badge.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { clsx } from "clsx";
+
+const Badge = ({
+  text,
+  height = "h-7",
+  backgroundColor = "bg-ELSE-FF3",
+  textColor = "text-SYSTEM-main",
+  textSize = "text-sm",
+  hasIcon = false,
+  iconWidth = 12,
+  iconSrc,
+}: BadgeProps) => {
+  return (
+    <div
+      className={clsx(
+        "flex items-center gap-1 rounded-sm px-1 w-fit",
+        height,
+        backgroundColor
+      )}
+    >
+      {hasIcon && iconSrc && (
+        <Image
+          src={iconSrc}
+          alt="badge-icon"
+          width={iconWidth}
+          height={iconWidth}
+        />
+      )}
+      <span className={clsx(textColor, textSize)}>{text}</span>
+    </div>
+  );
+};
+
+export default Badge;
--- src/components/common/types.d.ts
+++ src/components/common/types.d.ts
@@ -1,1 +1,12 @@
 type InputProps = Omit<React.ComponentPropsWithoutRef<"input">, "type" | "id">;
+
+type BadgeProps = {
+  text: string;
+  height: "h-[18px]" | "h-7";
+  backgroundColor: "bg-SYSTEM-main" | "bg-ELSE-FF3" | "bg-ELSE-F0";
+  textColor: "text-SYSTEM-white" | "text-SYSTEM-main" | "text-ELSE-C1";
+  textSize: "text-sm" | "text-lg";
+  hasIcon: boolean;
+  iconSrc?: string;
+  iconWidth?: number;
+};
