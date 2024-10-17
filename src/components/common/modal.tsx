08af4e5b3328b49b0c44d1ed7f6e653683ac2a78
재희 <hareppy329@gmail.com>
2024-10-18 05:27:52 +0900

    ♻️ WOW-28 refactor : 코드리뷰 반영하여 리팩토링

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -20,12 +20,7 @@ const Modal = (props: ModalProps) => {
 
   // 모달 열릴 때 외부 스크롤 차단
   useEffect(() => {
-    if (isShow) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "";
-    }
-
+    document.body.style.overflow = isShow ? "hidden" : "";
     return () => {
       document.body.style.overflow = ""; // 컴포넌트 언마운트 시 복원
     };
@@ -37,47 +32,43 @@ const Modal = (props: ModalProps) => {
     }
   };
 
+  if (!isShow) return null; // 모달이 닫혀있을 경우 렌더링 방지
+
   return (
-    <div>
-      {isShow && (
-        <div
-          id="common-popup-modal"
-          tabIndex={-1}
-          className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-ELSE-A1 bg-opacity-50"
-          onClick={handleOutsideClick} // 외부 클릭 시 모달 닫기
-        >
-          <div
-            className={twMerge(
-              "relative px-4 py-6 w-[343px] max-h-full bg-white rounded-lg",
-              textAlign
-            )}
-          >
-            {icon && (
-              <div className="flex justify-center mb-4">
-                <Image src={icon} alt="Icon" width={56} height={56} />
-              </div>
-            )}
-            <h1 className="mb-5 text-xl font-bold text-SYSTEM-black">
-              {title}
-            </h1>
-            <h3 className="mb-5 text-lg text-ELSE-55">{content}</h3>
-            <div className="flex w-full h-14">
-              {optionalBtnText && (
-                <CustomButton
-                  variant="outline"
-                  className="mr-3"
-                  onClick={handleOptional}
-                >
-                  {optionalBtnText}
-                </CustomButton>
-              )}
-              <CustomButton variant="filled" onClick={handleAction}>
-                {btnText}
-              </CustomButton>
-            </div>
+    <div
+      id="common-popup-modal"
+      tabIndex={-1}
+      className="fixed inset-0 z-50 flex justify-center items-center w-full h-full bg-ELSE-A1 bg-opacity-50"
+      onClick={handleOutsideClick} // 외부 클릭 시 모달 닫기
+    >
+      <div
+        className={twMerge(
+          "relative px-4 py-6 w-[343px] max-h-full bg-white rounded-lg",
+          textAlign
+        )}
+      >
+        {icon && (
+          <div className="flex justify-center mb-4">
+            <Image src={icon} alt="Icon" width={56} height={56} />
           </div>
+        )}
+        <h1 className="mb-5 text-xl font-bold text-SYSTEM-black">{title}</h1>
+        <h3 className="mb-5 text-lg text-ELSE-55">{content}</h3>
+        <div className="flex w-full h-14">
+          {optionalBtnText && (
+            <CustomButton
+              variant="outline"
+              className="mr-3"
+              onClick={handleOptional}
+            >
+              {optionalBtnText}
+            </CustomButton>
+          )}
+          <CustomButton variant="filled" onClick={handleAction}>
+            {btnText}
+          </CustomButton>
         </div>
-      )}
+      </div>
     </div>
   );
 };
