bf853601fa6dcade6d0fe7475501efac697122fb
hayoung <wendy4332@naver.com>
2024-10-18 20:44:04 +0900

    ➕ WOW-35 feat : 위시 리스트 컴포넌트 생성

---
+++ src/components/wish-list/wish-list-nonecontents.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import heartIcon from "@/assets/icons/nav_wishList_gray.svg";
+import { createTranslation } from "@/utils/localization/server";
+
+const WishListNoContents = async ({
+  params: { locale },
+}: WishListNoContentsProps) => {
+  const { t } = await createTranslation(locale, "common");
+  return (
+    <div className="grid  place-items-center w-[343px] h-[478px]">
+      <div className="grid  place-items-center w-[343px] h-[154px] ">
+        <Image src={heartIcon} alt="heartIcon" width={80} height={80} />
+        <div className="grid grid-cols-1 gap-[6px] text-center ">
+          <p className="text-lg text-ELSE-55">{t("wishListNoContents")}</p>
+          <p className="text-md text-ELSE-F8">
+            {t("wishLostNoContentsDescription")}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default WishListNoContents;
