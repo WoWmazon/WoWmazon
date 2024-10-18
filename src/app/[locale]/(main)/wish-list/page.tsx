562f696461f0a2bddd0be82a8b2d36b2340b47ae
hayoung <wendy4332@naver.com>
2024-10-18 20:44:28 +0900

    ➕ WOW-35 feat : wish list 페이지 생성

--- src/app/[locale]/(main)/wish-list/page.tsx
+++ src/app/[locale]/(main)/wish-list/page.tsx
@@ -1,4 +1,13 @@
-const page = () => {
-  return <div>wish list page</div>;
+import WishListHeader from "@/components/wish-list/wish-list-header";
+import WishListNoContents from "@/components/wish-list/wish-list-nonecontents";
+
+const page = ({ params: { locale } }: WishListNoContentsProps) => {
+  const number = 5; //예시
+  return (
+    <div className="grid gap-4">
+      <WishListHeader wishListNumber={number} />
+      <WishListNoContents params={{ locale }} />
+    </div>
+  );
 };
 export default page;
