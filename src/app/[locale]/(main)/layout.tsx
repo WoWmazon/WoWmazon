157fed4f1c705956327eabe011c1eb9fe462fe74
재희 <hareppy329@gmail.com>
2024-10-22 05:53:35 +0900

    🔧 WOW-38 fix : scroll 제거로 필요없어진 css 제거

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -6,7 +6,7 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="pb-16 min-h-full h-fit">
+    <div className="pb-16">
       {children}
       <BottomNav />
     </div>
