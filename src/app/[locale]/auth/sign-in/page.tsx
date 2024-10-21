8aa86424378bbdbcf62e1c98e90500b8dca3ba67
ljs614 <sjl0614@gmail.com>
2024-10-21 20:02:37 +0900

    ➕ WOW-31 feat: 리프레시토큰 만료 시 로그인 추가

--- src/app/[locale]/auth/sign-in/page.tsx
+++ src/app/[locale]/auth/sign-in/page.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
 
 import NitoLogo from "@/assets/icons/logo_white.svg";
-import { getCookie } from "@/utils/cookie";
+import { getCookie, setCookie } from "@/utils/cookie";
+import { isUndefined } from "@/utils/type-guard";
+import { redirect } from "next/navigation";
 
 const SignInPage = async () => {
   const device = getCookie("device");
   const refreshToken = getCookie("refreshToken");
 
-  // const res = await fetch(
-  //   `${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/user/login`,
-  //   {
-  //     method: "POST",
-  //     cache: "no-store",
-  //     body: JSON.stringify({ device, refreshToken }),
-  //   }
-  // ).then((res) => res.json());
+  const {
+    accessToken,
+    refreshToken: newRefreshToken,
+    error,
+  } = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/user/login`, {
+    method: "POST",
+    cache: "no-store",
+    body: JSON.stringify({ device, refreshToken }),
+  }).then((res) => res.json());
+
+  if (!isUndefined(error)) {
+    setCookie("accessToken", accessToken);
+    setCookie("refreshToken", newRefreshToken);
+    console.log("signin");
+    redirect("/");
+  }
 
   return (
     <div className="absolute flex flex-col top-0 left-0 size-full bg-SYSTEM-main bg-signin text-SYSTEM-white">
