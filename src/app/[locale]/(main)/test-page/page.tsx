f67d059eecd3ad520568a0b916714bc0831089f5
재희 <hareppy329@gmail.com>
2024-10-21 18:14:42 +0900

    💄 WOW-38 design : layout padding 제거로 테스트 페이지 padding 추가

--- src/app/[locale]/(main)/test-page/page.tsx
+++ src/app/[locale]/(main)/test-page/page.tsx
@@ -12,35 +12,37 @@ export default async function Home({
   const { t } = await createTranslation(locale, "common");
 
   return (
-    <div className="test-container">
-      {/* 다국어 처리 테스트 */}
-      <h1>{t("greeting")}</h1>
-      <h3>{t("description")}</h3>
-      {/* modal button */}
-      <TestButton />
-      <br />
-      {/* custom button */}
-      <div className="flex flex-col gap-2">
-        <CustomButton variant="disabled">disabled button</CustomButton>
-        <CustomButton variant="filled">filled button</CustomButton>
-        <CustomButton smallSize variant="outline">
-          outline button
-        </CustomButton>
-        <CustomButton smallSize variant="outlineColor">
-          filled button
-        </CustomButton>
+    <div className="test-container relative h-full mb-[64px]">
+      <div className="p-4">
+        {/* 다국어 처리 테스트 */}
+        <h1>{t("greeting")}</h1>
+        <h3>{t("description")}</h3>
+        {/* modal button */}
+        <TestButton />
+        <br />
+        {/* custom button */}
+        <div className="flex flex-col gap-2">
+          <CustomButton variant="disabled">disabled button</CustomButton>
+          <CustomButton variant="filled">filled button</CustomButton>
+          <CustomButton smallSize variant="outline">
+            outline button
+          </CustomButton>
+          <CustomButton smallSize variant="outlineColor">
+            filled button
+          </CustomButton>
+        </div>
+        <br />
+        <div className="grid grid-cols-[250px_auto] gap-1">
+          <input
+            placeholder="닉네임을 입력해주세요"
+            className="border border-bg-black h-14 px-4 py-[14px] bg-SYSTEM-white"
+          />
+          <CustomButton size="large" variant="outlineColor">
+            중복확인
+          </CustomButton>
+        </div>
+        <TestComponents />
       </div>
-      <br />
-      <div className="grid grid-cols-[250px_auto] gap-1">
-        <input
-          placeholder="닉네임을 입력해주세요"
-          className="border border-bg-black h-14 px-4 py-[14px] bg-SYSTEM-white"
-        />
-        <CustomButton size="large" variant="outlineColor">
-          중복확인
-        </CustomButton>
-      </div>
-      <TestComponents />
     </div>
   );
 }
