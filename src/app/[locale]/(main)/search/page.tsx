dda44573f695966d2757e6d7889871c624c6b000
ljs614 <sjl0614@gmail.com>
2024-10-23 05:06:53 +0900

    🔧 WOW-43 fix: 빌드 ESLint 에러 수정

--- src/app/[locale]/(main)/search/page.tsx
+++ src/app/[locale]/(main)/search/page.tsx
@@ -1,13 +1,13 @@
-import RecentSearchKeyword from "@/components/search/recent-search-keyword";
+// import RecentSearchKeyword from "@/components/search/recent-search-keyword";
 import SearchHeader from "@/components/search/search-header";
 import SearchResult from "@/components/search/search-result";
 
 const page = () => {
   // 예시
-  const keywords = [
-    { id: "1", keyword: "우유" },
-    { id: "2", keyword: "모니터" },
-  ];
+  // const keywords = [
+  //   { id: "1", keyword: "우유" },
+  //   { id: "2", keyword: "모니터" },
+  // ];
 
   return (
     <div className="px-4 text-ELSE-33">
