80e5af8285e67132a74ddb35e8cd74dd8b42c85a
ljs614 <sjl0614@gmail.com>
2024-10-24 18:19:44 +0900

    ♻️  WOW-44 refactor: any 제거하여 함수 수정

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,22 +1,11 @@
 import { URLSearchParams } from "url";
 
-export const createQueryString = (obj: Record<string, any>) => {
-  const hasOnlyStringValue = Object.values(obj).every(
-    (value) => typeof value !== "string"
+type QueryObject = Record<string, string | number | boolean>;
+
+export const createQueryString = (obj: QueryObject): URLSearchParams => {
+  const allEntriesAsStrings: [string, string][] = Object.entries(obj).map(
+    ([key, value]) => [key, String(value)]
   );
-  if (hasOnlyStringValue) {
-    return new URLSearchParams(obj);
-  }
 
-  const stringObject = { ...obj };
-
-  Object.entries(obj).forEach(([key, value]) => {
-    if (typeof value !== "string") {
-      stringObject[key] = `${value}`;
-      return;
-    }
-    stringObject[key] = value;
-  });
-
-  return new URLSearchParams(stringObject);
+  return new URLSearchParams(allEntriesAsStrings);
 };
