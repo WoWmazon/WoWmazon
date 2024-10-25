cc6efc5ec078eeddf78e4f6946a35b9a00d3e628
ljs614 <sjl0614@gmail.com>
2024-10-25 23:38:32 +0900

    ♻️ WOW-46 refactor: postRegisterUser 구조 수정

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,7 +5,7 @@ import { createDeviceInfo } from "@/utils/deviceUtils";
 import { LocaleTypes } from "@/utils/localization/settings";
 import { createRegisterBody } from "@/utils/registerUtils";
 import { isUndefined } from "@/utils/type-guard";
-import { API_USER_REGISTER_URL } from "@/constants/api-urls";
+import { NITO_USER_REGISTER_URL } from "@/constants/nito-urls";
 
 export const postRegisterUser = async (
   data: FormInput,
@@ -19,7 +19,7 @@ export const postRegisterUser = async (
       deviceInfo,
     });
 
-    const res = await fetch(API_USER_REGISTER_URL, {
+    const resonse = await fetch(NITO_USER_REGISTER_URL, {
       method: "POST",
       cache: "no-store",
       body: JSON.stringify(registerBody),
@@ -28,19 +28,24 @@ export const postRegisterUser = async (
       },
     });
 
-    if (!res.ok) {
-      throw new Error(`Failed to register: ${res.status} - ${res.statusText}`);
+    if (!resonse.ok) {
+      throw new Error(
+        `Failed to register: ${resonse.status} - ${resonse.statusText}`
+      );
     }
 
-    const { accessToken, refreshToken, error } = await res.json();
+    const { accessToken, refreshToken, error } = await resonse.json();
 
     if (isUndefined(error)) {
-      setCookie("accessToken", accessToken, { httpOnly: true, secure: true });
-      setCookie("refreshToken", refreshToken, { httpOnly: true, secure: true });
-      setCookie("device", JSON.stringify(deviceInfo), {
-        httpOnly: true,
+      const maxAge = 60 * 60 * 24 * 365; // 1 year in seconds
+      const setCookieOptions = {
         secure: true,
-      });
+        maxAge,
+      };
+
+      setCookie("accessToken", accessToken, setCookieOptions);
+      setCookie("refreshToken", refreshToken, setCookieOptions);
+      setCookie("device", JSON.stringify(deviceInfo), setCookieOptions);
     } else {
       throw new Error(`Registration error: ${error}`);
     }
