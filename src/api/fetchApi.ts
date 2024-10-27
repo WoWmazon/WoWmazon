616e16f05f21a00c06afd54c57e0d3e506f29952
재희 <hareppy329@gmail.com>
2024-10-28 06:11:22 +0900

    ♻️ WOW-41 refactor: fetchAPI response type 받도록 수정

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -5,12 +5,11 @@ import { getCookie } from "@/utils/cookie";
 const NITO_BASE_URL = process.env.NEXT_PUBLIC_NITO_URL;
 const token = getCookie("accessToken");
 
-//token 값이 필요한 fetch 함수
-export const fetchWithToken = async (
+export async function fetchWithToken<T = any>(
   endpoint: string,
   options: RequestInit = {},
   queryParams?: Record<string, string> // 쿼리 파라미터를 위한 인수 추가
-) => {
+) {
   const url = createURLWithParams(`${NITO_BASE_URL}`, endpoint, queryParams);
 
   const response = await fetch(url, {
@@ -26,15 +25,15 @@ export const fetchWithToken = async (
   if (!response.ok) {
     throw new Error(`API 요청 실패: ${response.statusText}`);
   }
-  return response.json();
-};
+  return response.json() as T;
+}
 
 //token 값 필요없을때 fetch 함수
-export const fetchWithNoToken = async (
+export async function fetchWithNoToken<T = any>(
   endpoint: string,
   options: RequestInit,
   queryParams?: Record<string, string>
-) => {
+) {
   //쿼리파라미터가 있을때 url 처리
   const url = createURLWithParams(`${NITO_BASE_URL}`, endpoint, queryParams);
   const response = await fetch(url, {
@@ -45,5 +44,5 @@ export const fetchWithNoToken = async (
     },
     credentials: "include",
   });
-  return response.json();
-};
+  return response.json() as T;
+}
