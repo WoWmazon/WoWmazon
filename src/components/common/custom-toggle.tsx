bcbaecc2b5ccfa538c70307789be1facea7a8aef
ljs614 <sjl0614@gmail.com>
2024-10-29 15:22:45 +0900

    ♻️ WOW-51 refactor: custom-toggle 불필요한 div 제거

--- src/components/common/custom-toggle.tsx
+++ src/components/common/custom-toggle.tsx
@@ -7,25 +7,23 @@ const CustomToggle = (props: InputOmitProps) => {
   const uuid = useId();
 
   return (
-    <div>
-      <label className="inline-flex items-center cursor-pointer" htmlFor={uuid}>
-        <input
-          type="checkbox"
-          className="sr-only peer"
-          id={uuid}
-          disabled={disabled}
-          {...rest}
-        />
-        <div
-          className={twMerge(
-            "relative w-[50px] h-[30px] bg-ELSE-AE rounded-full peer peer-checked:after:translate-x-3/4 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-[26px] after:w-[26px] after:transition-all peer-checked:bg-SYSTEM-main",
-            disabled && "peer-checked:bg-ELSE-AE",
-            className
-          )}
-        ></div>
-        {children}
-      </label>
-    </div>
+    <label className="inline-flex items-center cursor-pointer" htmlFor={uuid}>
+      <input
+        type="checkbox"
+        className="sr-only peer"
+        id={uuid}
+        disabled={disabled}
+        {...rest}
+      />
+      <div
+        className={twMerge(
+          "relative w-[50px] h-[30px] bg-ELSE-AE rounded-full peer peer-checked:after:translate-x-3/4 after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-[26px] after:w-[26px] after:transition-all peer-checked:bg-SYSTEM-main",
+          disabled && "peer-checked:bg-ELSE-AE",
+          className
+        )}
+      ></div>
+      {children}
+    </label>
   );
 };
 
