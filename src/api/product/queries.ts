ef430e37969caff7b87fd3fa9f89680ac6c16f0f
ljs614 <sjl0614@gmail.com>
2024-10-29 13:52:29 +0900

    ♻️ WOW-47 refactor: useSearchProducts enabled 조건 수정

--- src/api/product/queries.ts
+++ src/api/product/queries.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProductListBySearch } from "./apis";
+import { isUndefined } from "@/utils/type-guard";
 
 export const useSearchProducts = (data: ProductParamsType) =>
   useQuery({
     queryKey: ["searchProduct", data],
     queryFn: () => getProductListBySearch(data),
-    enabled: !!data.search,
+    enabled: !isUndefined(data?.search) && data.search !== "",
   });
