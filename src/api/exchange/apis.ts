7cbca263ad7ad107767a3c1ee7538ac458f38626
hayoung78 <wendy4332@naver.com>
2024-10-30 20:51:49 +0900

    🔥 WOW-48 remove : error 수정

--- src/api/exchange/apis.ts
+++ src/api/exchange/apis.ts
@@ -5,7 +5,6 @@ import { fetchWithToken } from "../fetchApi";
 //환율 api
 export const getExchangeLatest = async () => {
   const data = await fetchWithToken<GetExchangeResponse>("exchange/latest/", {
-    method: "GET",
     next: { revalidate: 18000 }, // 5시간
   });
   return data;
