9b8f3590b65ce06c2d5611f75a9e50aa62467c63
hayoung78 <wendy4332@naver.com>
2024-11-19 16:35:47 +0900

    ➕  WOW-67 feat : api 요청전 유효한토큰으로 미리갱신하는 함수 생성

--- src/api/fetchApi.ts
+++ src/api/fetchApi.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { createURLWithParams } from "@/utils/apis/create-URL-Params";
-import { getCookie } from "@/utils/cookie";
+import { getValidAccessToken } from "@/auth/token";
 
 const NITO_BASE_URL = process.env.NEXT_PUBLIC_NITO_URL;
 
@@ -11,22 +11,25 @@ export const fetchWithToken = async <T>(
   queryParams?: Record<string, string> // 쿼리 파라미터를 위한 인수 추가
 ) => {
   const url = createURLWithParams(`${NITO_BASE_URL}`, endpoint, queryParams);
-  const token = getCookie("accessToken");
 
-  const response = await fetch(url, {
-    method: options.method || "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-      ...options.headers, // 추가 헤더 병합
-    },
-    credentials: "include", // 자격증명 포함
-    ...options,
-  });
-  if (!response.ok) {
-    throw new Error(`API 요청 실패: ${response.statusText}`);
+  const token = await getValidAccessToken();
+
+  if (token) {
+    const response = await fetch(url, {
+      method: options.method || "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        ...options.headers, // 추가 헤더 병합
+      },
+      credentials: "include", // 자격증명 포함
+      ...options,
+    });
+    if (!response.ok) {
+      throw new Error(`API 요청 실패: ${response.statusText}`);
+    }
+    return response.json() as T;
   }
-  return response.json() as T;
 };
 
 // token 값 필요없을 때 fetch 함수
