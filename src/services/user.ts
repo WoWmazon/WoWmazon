721c8935126b3776a2aa0f540510e23ee5d66c3e
ljs614 <sjl0614@naver.com>
2024-11-19 14:21:23 +0900

    🔧 WOW-59 fix: 토큰 리프레시 에러 및 무한 요청 수정

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,11 +1,14 @@
-import { fetchServer } from "@/api/fetch.server";
+const NITO_BASE_URL = process.env.NEXT_PUBLIC_NITO_URL;
 
 export const postUserRefresh = async (refreshToken: string) => {
-  return fetchServer("user/refresh/", {
+  // refresh를 fetchServer로 요청할 시 오류가 나면 무한 요청이 발생함.
+  // refresh 시 만료된 access token을 보내면 안 됨.
+  return fetch(`${NITO_BASE_URL}/user/refresh/`, {
     method: "POST",
     body: JSON.stringify({ refreshToken }),
     headers: {
       accept: "application/json",
+      "Content-Type": "application/json",
     },
   });
 };
