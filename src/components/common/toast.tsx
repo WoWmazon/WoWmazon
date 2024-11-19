0d33b4a40852ba9b18bf3e8db8d053b81cb9928e
ljs614 <sjl0614@naver.com>
2024-11-19 17:12:55 +0900

    🔧 WOW-69 fix: toast 이상 동작 수정

--- src/components/common/toast.tsx
+++ src/components/common/toast.tsx
@@ -1,31 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 const Toast = ({
   message,
-  open,
   onChange,
   error = false,
   autoHideDuration = 3000,
 }: ToastProps) => {
   const [isOpen, setIsOpen] = useState(true);
 
-  setTimeout(async () => {
-    setIsOpen(false);
-    await new Promise((resolve) => {
-      setTimeout(resolve, 450);
-    });
-    onChange(false);
-  }, autoHideDuration);
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      setIsOpen(false);
+      await new Promise((resolve) => {
+        setTimeout(resolve, 450);
+      });
+      onChange(false);
+    }, autoHideDuration);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [message, autoHideDuration, error, onChange]);
 
   return (
     <div className="absolute flex justify-center left-0 bottom-0 w-full sm:w-[375px]">
       <div
         className={twMerge(
           "fixed bottom-0 h-[52px] w-[343px] text-center content-center bg-ELSE-33 text-SYSTEM-white text-md z-30",
-          open && isOpen && "animate-slideUp",
+          isOpen && "animate-slideUp",
           !isOpen && "animate-slideDown",
           error && "bg-ELSE-FF2 text-ELSE-F60"
         )}
