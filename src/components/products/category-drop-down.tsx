198ca8160852519c346b65e11d69f224af5271d8
hayoung78 <wendy4332@naver.com>
2024-11-22 05:05:48 +0900

    ➕ WOW-73 feat : category 클릭후 다시 드롭다운 열때 선택된 카테고리 항목으로 자동 스크롤 기능 추가

--- src/components/products/category-drop-down.tsx
+++ src/components/products/category-drop-down.tsx
@@ -8,12 +8,13 @@ const CategoryDropDown = ({ categories, onSelect }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const [activeCategory, setActiveCategory] = useState<string | null>("All");
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const categoryRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   const toggleDropdown = () => {
     setIsOpen((prev) => !prev);
   };
 
-  const handleSelect = (category: string) => {
+  const handleSelect = (category: string, idx: number) => {
     setActiveCategory(category);
     onSelect(category);
     setIsOpen(false);
@@ -35,6 +36,21 @@ const CategoryDropDown = ({ categories, onSelect }: DropdownProps) => {
     };
   }, []);
 
+  // 드롭다운 열릴 때 선택된 항목으로 스크롤
+  useEffect(() => {
+    if (isOpen && activeCategory) {
+      const activeIndex = categories.findIndex(
+        (category) => category === activeCategory
+      );
+      if (activeIndex === -1) return;
+      const scrollOptions: ScrollIntoViewOptions = {
+        behavior: "smooth",
+        block: activeIndex === categories.length - 1 ? "nearest" : "center",
+      };
+      categoryRefs.current[activeIndex]?.scrollIntoView(scrollOptions);
+    }
+  }, [isOpen, activeCategory, categories]);
+
   return (
     <div className="relative" ref={dropdownRef}>
       {/* 드롭다운 버튼 */}
@@ -54,14 +70,21 @@ const CategoryDropDown = ({ categories, onSelect }: DropdownProps) => {
       </div>
       {/* 드롭다운 메뉴 */}
       {isOpen && (
-        <div className="absolute  w-[343px] h-[352px] overflow-auto shadow-lg rounded-sm z-10 bg-SYSTEM-white">
+        <div className="absolute  w-[343px] max-h-[352px] overflow-y-auto shadow-lg rounded-sm z-10 bg-SYSTEM-white">
           {categories.map((category, idx) => (
-            <CategoryDropDownButton
+            <div
               key={idx}
-              label={category}
-              isActive={activeCategory === category}
-              onClick={() => handleSelect(category)}
-            />
+              ref={(el) => {
+                categoryRefs.current[idx] = el;
+                return undefined;
+              }}
+            >
+              <CategoryDropDownButton
+                label={category}
+                isActive={activeCategory === category}
+                onClick={() => handleSelect(category, idx)}
+              />
+            </div>
           ))}
         </div>
       )}
