50dbf361029772f362e1b38cd840cad74cc9b569
재희 <hareppy329@gmail.com>
2024-11-26 05:51:29 +0900

    ➕ WOW-63 feat : editAlarm useMutation 분리

--- src/hooks/useFavoriteProduct.ts
+++ src/hooks/useFavoriteProduct.ts
@@ -9,7 +9,7 @@ import {
   useMutation,
   useQueryClient,
 } from "@tanstack/react-query";
-import { WISH_LIST } from "@/constants/query-keys";
+import { PRODUCT_DETAIL, WISH_LIST } from "@/constants/query-keys";
 import { useToastStore } from "@/stores/common/stores";
 
 export const useFavoriteProductList = (params: FavoriteProductParamsType) => {
@@ -37,7 +37,7 @@ export const useSetFavoriteProduct = (queryKey?: string[]) => {
   const { handleToast } = useToastStore();
 
   const { mutateAsync: addWishList } = useMutation({
-    mutationFn: ( id : number ) => postFavoriteProduct(id),
+    mutationFn: (id: number) => postFavoriteProduct(id),
     onSuccess: () => {
       if (queryKey) queryClient.invalidateQueries({ queryKey });
       handleToast({
@@ -58,9 +58,9 @@ export const useSetFavoriteProduct = (queryKey?: string[]) => {
   });
 
   const { mutateAsync: deleteWishList } = useMutation({
-    mutationFn: ({ id }: { id: number }) => deleteFavoriteProduct(id),
+    mutationFn: (id: number) => deleteFavoriteProduct(id),
     onSuccess: () => {
-      if (queryKey) queryClient.invalidateQueries({ queryKey: ["WISH_LIST"] });
+      if (queryKey) queryClient.invalidateQueries({ queryKey });
       handleToast({
         open: true,
         onChange: () => handleToast({ open: false }),
@@ -78,12 +78,22 @@ export const useSetFavoriteProduct = (queryKey?: string[]) => {
     },
   });
 
+  return {
+    addWishList,
+    deleteWishList,
+  };
+};
+
+export const useSetAlarm = (queryKey?: string[]) => {
+  const queryClient = useQueryClient();
+  const { handleToast } = useToastStore();
+
   const { mutate: editAlarm } = useMutation({
     mutationFn: ({ id, isAlarm }: { id: number; isAlarm: boolean }) =>
       putFavoriteProduct(id, isAlarm),
-    onMutate: (newData) => {
+    onMutate: () => {
       const previousData = queryClient.getQueryData<FavoriteProductList>([
-        "WISH_LIST",
+        PRODUCT_DETAIL,
       ]);
 
       const obj: FavoriteProductList | undefined = previousData && {
@@ -93,24 +103,24 @@ export const useSetFavoriteProduct = (queryKey?: string[]) => {
 
       if (previousData) {
         // 낙관적 업데이트
-        queryClient.setQueryData<FavoriteProductList>(["WISH_LIST"], obj);
+        queryClient.setQueryData<FavoriteProductList>([PRODUCT_DETAIL], obj);
       }
       return { previousData };
     },
-    onSuccess: (data: FavoriteProductList | undefined, newData) => {
-      queryClient.invalidateQueries({ queryKey: ["WISH_LIST"] });
+    onSuccess: (data: FavoriteProductList | undefined) => {
+      if (queryKey) queryClient.invalidateQueries({ queryKey });
       handleToast({
         open: true,
         onChange: () => handleToast({ open: false }),
         message: data?.isAlarm
-          ? "찜하기 추가되었습니다."
-          : "찜하기 삭제되었습니다",
+          ? "알림이 설정되었습니다."
+          : "알림이 해제되었습니다",
       });
     },
-    onError: (error, newData, context) => {
+    onError: (error, params, context) => {
       if (error) {
         if (context) {
-          queryClient.setQueryData(["WISH_LIST"], context.previousData);
+          queryClient.setQueryData([PRODUCT_DETAIL], context.previousData);
         }
 
         handleToast({
@@ -123,8 +133,6 @@ export const useSetFavoriteProduct = (queryKey?: string[]) => {
   });
 
   return {
-    addWishList,
-    deleteWishList,
     editAlarm,
   };
 };
