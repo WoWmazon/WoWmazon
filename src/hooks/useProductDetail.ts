4abdc0a7b4fabe0cc75e52c982e8dc27a403d0d7
재희 <hareppy329@gmail.com>
2024-11-26 05:53:38 +0900

    ➕ WOW-63 feat : product detail useQuery 생성

---
+++ src/hooks/useProductDetail.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { PRODUCT_DETAIL, RELATED_PRODUCT } from "@/constants/query-keys";
+import { getProductDetail, getRelatedProductList } from "@/api/product/apis";
+
+export const useProductDetail = (id: string) => {
+  return useQuery<GetProductDetailResponse | undefined>({
+    queryKey: [PRODUCT_DETAIL, id],
+    queryFn: async () => await getProductDetail(id),
+    staleTime: 60 * 60 * 1000,
+  });
+};
+
+export const useRelatedProduct = (id: string) => {
+  return useQuery<ProductResultType[] | undefined>({
+    queryKey: [RELATED_PRODUCT, id],
+    queryFn: async () => await getRelatedProductList(id),
+    staleTime: 60 * 60 * 1000,
+  });
+};
