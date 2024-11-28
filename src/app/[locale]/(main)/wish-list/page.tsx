6fa1289266750d8a3c86e368c79a2f90d1942ad9
ljs614 <sjl0614@naver.com>
2024-11-28 23:44:44 +0900

    ♻️ WOW-82 refactor: 찜한 상품 prefetch 적용

--- src/app/[locale]/(main)/wish-list/page.tsx
+++ src/app/[locale]/(main)/wish-list/page.tsx
@@ -1,6 +1,42 @@
+import { getFavoriteProduct } from "@/api/favorite/apis";
 import WishListContainer from "@/components/wish-list/wish-list-container";
+import { WISH_LIST } from "@/constants/query-keys";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const page = () => {
-  return <WishListContainer />;
+const params: { ordering: "product_priority" } = {
+  ordering: "product_priority",
+};
+
+const page = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [WISH_LIST, params],
+    queryFn: ({ pageParam = "" }) => {
+      const queryParams: FavoriteProductParamsType = {
+        ...params,
+        cursor: pageParam,
+      };
+      return getFavoriteProduct(queryParams);
+    },
+    initialPageParam: "",
+    getNextPageParam: (lastPage: GetFavoriteProductList) => {
+      if (!lastPage || !lastPage.cursor) {
+        return undefined;
+      }
+      return lastPage.cursor;
+    },
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <WishListContainer />
+    </HydrationBoundary>
+  );
 };
 export default page;
