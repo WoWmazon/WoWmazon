8a70890d68eff3166ceff26efb852e5c6606eee2
ljs614 <sjl0614@naver.com>
2024-11-29 00:09:09 +0900

    🔧 WOW-82 fix: 편집상태에서 카드 위쪽클릭 시 디테일 페이지로 넘어가는 버그 수정

--- src/components/wish-list/wish-list-product-card.tsx
+++ src/components/wish-list/wish-list-product-card.tsx
@@ -10,14 +10,16 @@ const WishListProductCard = ({
   return (
     <div className="relative">
       {isEditing && (
-        <div
-          className={twMerge(
-            "absolute w-full h-32 top-2 bg-SYSTEM-white bg-opacity-70",
-            isChecked && "bg-SYSTEM-main bg-opacity-20"
-          )}
-        >
-          <div className="mt-4 size-20 justify-items-center content-center">
-            <CustomCheckBox checked={isChecked} onChange={onCheck} large />
+        <div className="absolute size-full bg-SYSTEM-white bg-opacity-70">
+          <div
+            className={twMerge(
+              "w-full h-32 mt-2",
+              isChecked && "bg-SYSTEM-main bg-opacity-20 "
+            )}
+          >
+            <div className="size-20 justify-items-center content-center">
+              <CustomCheckBox checked={isChecked} onChange={onCheck} large />
+            </div>
           </div>
         </div>
       )}
