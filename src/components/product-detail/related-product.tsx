018c0a6d6f4f277891b528c0a0d34f739b599492
재희 <hareppy329@gmail.com>
2024-12-05 01:29:39 +0900

    🔧 WOW-85 fix : present price가 null인 list만 올 경우 연관된 상품 타이틀만 보이는 오류 컴포넌트 자체가 안보이도록 수정

--- src/components/product-detail/related-product.tsx
+++ src/components/product-detail/related-product.tsx
@@ -18,26 +18,30 @@ const RelatedProduct = ({
     return null;
   }
 
+  const validData = relatedProducts.filter(
+    (product) => product.presentPrice !== null
+  );
+
+  if (validData.length === 0) return null;
+
   return (
     <div className="bg-SYSTEM-white">
       <div className="px-4 py-[30px]">
         <p className="font-bold mb-3">해당 상품과 비슷한 상품</p>
         <div className="w-full flex gap-3 overflow-x-auto">
-          {relatedProducts
-            .filter((product) => product.presentPrice !== null)
-            .map((item) => {
-              return isLoading ? (
-                <RelatedProductCardSkeleton />
-              ) : (
-                exchangeRate && (
-                  <RelatedProductCard
-                    key={item.id}
-                    relatedProduct={item}
-                    exchangeRate={exchangeRate}
-                  />
-                )
-              );
-            })}
+          {validData.map((item) => {
+            return isLoading ? (
+              <RelatedProductCardSkeleton />
+            ) : (
+              exchangeRate && (
+                <RelatedProductCard
+                  key={item.id}
+                  relatedProduct={item}
+                  exchangeRate={exchangeRate}
+                />
+              )
+            );
+          })}
         </div>
       </div>
     </div>
